30be64be71a745f7ced4d97ecc51708d42468c8e
hamedblue1381 <hamed.balanar@gmail.com>
2023-12-29 19:53:18 +0330

    feat: add config loader

---
+++ util/config.go
@@ -0,0 +1,27 @@
+package util
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Environment       string `mapstructure:"ENVIRONMENT"`
+	DBDriver          string `mapstructure:"DB_DRIVER"`
+	DBSource          string `mapstructure:"DB_SOURCE"`
+	HTTPServerAddress string `mapstructure:"HTTP_SERVER_ADDRESS"`
+	TokenBot          string `mapstructure:"TOKEN_BOT"`
+	TelegramUrl       string `mapstructure:"TELEGRAM_URL"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
