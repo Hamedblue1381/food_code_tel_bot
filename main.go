b358fd1e59a948bd0a6bde92852d6ebebbc9757b
hamedblue1381 <hamed.balanar@gmail.com>
2024-01-02 10:30:14 +0330

    add authorization for keyboards

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func main() {
 
 	for update := range updatesChan {
 		if update.CallbackQuery != nil {
-			handleCallbackQuery(update.CallbackQuery, bot, conn)
+			handleCallbackQuery(update, update.CallbackQuery, bot, conn)
 			continue
 		}
 
@@ -67,52 +67,57 @@ func main() {
 	}
 }
 
-func handleCallbackQuery(callbackQuery *tgbotapi.CallbackQuery, bot *tgbotapi.BotAPI, conn *sql.DB) {
-	userSelection := userSelections[callbackQuery.Message.Chat.ID]
+func handleCallbackQuery(update tgbotapi.Update, callbackQuery *tgbotapi.CallbackQuery, bot *tgbotapi.BotAPI, conn *sql.DB) {
+	if callbackQuery.Message.Chat.ID == update.Message.Chat.ID {
+		userSelection := userSelections[callbackQuery.Message.Chat.ID]
 
-	if userSelection == nil {
-		userSelection = &models.UserSelection{}
-		userSelections[callbackQuery.Message.Chat.ID] = userSelection
-	}
-
-	data := callbackQuery.Data
-	callbackType := data[:3]
-	callbackMenu := data[3:6]
-	switch callbackMenu {
-	case "day":
-		day := data[7:]
-		userSelection.SelectedDay = day
-		msg := tgbotapi.NewMessage(callbackQuery.Message.Chat.ID, "Select a location:")
-		msg.ReplyMarkup = api.MakeLocationKeyboard(callbackType)
-		bot.Send(msg)
-	case "loc":
-		location := data[7:]
-		userSelection.SelectedLocation = location
-		msg := tgbotapi.NewMessage(callbackQuery.Message.Chat.ID, "Please type the food you'd like:")
-		msg.ReplyMarkup = api.MakeFoodKeyboard(callbackType)
-		bot.Send(msg)
-	case "eat":
-		food := data[7:]
-		userSelection.SelectedFood = food
-		err := api.InsertIntoDatabase(conn, callbackQuery.From.ID, userSelection, callbackType)
-		if err != nil {
-			msg := tgbotapi.NewMessage(callbackQuery.Message.Chat.ID, "Error saving your selection.")
-			bot.Send(msg)
-			log.Printf("Error inserting into database: %v", err)
-		} else {
-			msg := tgbotapi.NewMessage(callbackQuery.Message.Chat.ID, "Your selections have been saved!")
-			bot.Send(msg)
-			delete(userSelections, callbackQuery.Message.Chat.ID) // clear user state after successful operation
+		if userSelection == nil {
+			userSelection = &models.UserSelection{}
+			userSelections[callbackQuery.Message.Chat.ID] = userSelection
 		}
-	}
 
-	// Acknowledge the callback
-	callbackConfig := tgbotapi.CallbackConfig{
-		CallbackQueryID: callbackQuery.ID,
-		Text:            "",    // Optional text, not required for just an acknowledgment
-		ShowAlert:       false, // Determines if the response should be an alert or a notification at the top of the screen
-	}
-	if _, err := bot.Request(callbackConfig); err != nil {
-		log.Printf("Error responding to callback query: %v", err)
+		data := callbackQuery.Data
+		callbackType := data[:3]
+		callbackMenu := data[3:6]
+		switch callbackMenu {
+		case "day":
+			day := data[7:]
+			userSelection.SelectedDay = day
+			msg := tgbotapi.NewMessage(callbackQuery.Message.Chat.ID, "Select a location:")
+			msg.ReplyMarkup = api.MakeLocationKeyboard(callbackType)
+			bot.Send(msg)
+		case "loc":
+			location := data[7:]
+			userSelection.SelectedLocation = location
+			msg := tgbotapi.NewMessage(callbackQuery.Message.Chat.ID, "Please type the food you'd like:")
+			msg.ReplyMarkup = api.MakeFoodKeyboard(callbackType)
+			bot.Send(msg)
+		case "eat":
+			food := data[7:]
+			userSelection.SelectedFood = food
+			err := api.InsertIntoDatabase(conn, callbackQuery.From.ID, userSelection, callbackType)
+			if err != nil {
+				msg := tgbotapi.NewMessage(callbackQuery.Message.Chat.ID, "Error saving your selection.")
+				bot.Send(msg)
+				log.Printf("Error inserting into database: %v", err)
+			} else {
+				msg := tgbotapi.NewMessage(callbackQuery.Message.Chat.ID, "Your selections have been saved!")
+				bot.Send(msg)
+				delete(userSelections, callbackQuery.Message.Chat.ID) // clear user state after successful operation
+			}
+		}
+
+		// Acknowledge the callback
+		callbackConfig := tgbotapi.CallbackConfig{
+			CallbackQueryID: callbackQuery.ID,
+			Text:            "",    // Optional text, not required for just an acknowledgment
+			ShowAlert:       false, // Determines if the response should be an alert or a notification at the top of the screen
+		}
+		if _, err := bot.Request(callbackConfig); err != nil {
+			log.Printf("Error responding to callback query: %v", err)
+		}
+	} else {
+		msg := tgbotapi.NewCallbackWithAlert(callbackQuery.ID, "You are not allowed to use this keyboard")
+		bot.Send(msg)
 	}
 }
